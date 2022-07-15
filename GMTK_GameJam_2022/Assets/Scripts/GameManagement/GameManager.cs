45ff802a58a47b351993b35ed74a4afc77551030
BennXYZ <benny.zoeller@gmail.com>
2022-07-15 21:26:36 +0200

    Implement AutoRunning GameManager

--- GMTK_GameJam_2022/Assets/Scripts/GameManagement/GameManager.cs
+++ GMTK_GameJam_2022/Assets/Scripts/GameManagement/GameManager.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void GameStart() => DontDestroyOnLoad(Instantiate(Resources.Load("GameManager")));
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+
     }
 }
