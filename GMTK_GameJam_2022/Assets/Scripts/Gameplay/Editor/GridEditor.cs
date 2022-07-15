9d6194b41f6978875a0424eea543ad340f14b9e7
MaxPlay <maxplayyt@gmail.com>
2022-07-15 21:11:01 +0200

    Renamed SampleScene -> Main Added base classes for grid and its editor (mainly to create folders) Added Developer Folders with empty scenes for us to work in

---
+++ GMTK_GameJam_2022/Assets/Scripts/Gameplay/Editor/GridEditor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMTKJam2022.Gameplay.Editor
+{
+    public class GridEditor
+    {
+        // TODO(ms): Wait and see!
+    }
+}
---
+++ GMTK_GameJam_2022/Assets/Scripts/Gameplay/Grid.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace GMTKJam2022.Gameplay
+{
+    public class Grid : MonoBehaviour
+    {
+        // TODO(ms): Wait and see!
+    }
+}
