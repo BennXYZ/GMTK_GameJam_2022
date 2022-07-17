855f958259f055f28c4387e6e74ea2a8e2da865b
MaxPlay <maxplayyt@gmail.com>
2022-07-17 13:12:16 +0200

    Added camera movement logic  -> Still requires setup

---
+++ GMTK_GameJam_2022/Assets/Scripts/Gameplay/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GMTKJam2022.Gameplay
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        public CasinoGrid Grid { get; set; }
+        public PlayerEntity Player { get; set; }
+
+        private void Update()
+        {
+            Vector3 clampMin = new Vector3(Grid.Origin.x + 1.5f, 0, Grid.Origin.y + 1.5f);
+            Vector3 clampMax = new Vector3(Grid.Origin.x + Grid.Size.x - 1.5f, 0, Grid.Origin.y + Grid.Size.y - 1.5f);
+
+            transform.position = new Vector3(
+                Math.Clamp(Player.transform.position.x, clampMin.x, clampMax.x),
+                Math.Clamp(Player.transform.position.y, clampMin.y, clampMax.y),
+                Math.Clamp(Player.transform.position.z, clampMin.z, clampMax.z)
+                );
+        }
+    }
+}
